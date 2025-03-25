daa5dd5c1bce8cb1c58d51a9c20bfc6877d3c2d3
elcapitan88 <alexh6788@gmail.com>
2025-03-24 21:16:42 -0600

    fix warning

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   organizationName: 'atomiktrading',
   projectName: 'docs',
   
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   
   // Even if you don't use internationalization, you can use this field to set useful
